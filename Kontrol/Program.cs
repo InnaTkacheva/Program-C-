20beed9df36a4a11b986640bfc3b37f52e4baa90
Inna <inna-tum@ya.ru>
2022-05-09 00:23:01 +0700

    code for kontrol add 2

--- Kontrol/Program.cs
+++ Kontrol/Program.cs
@@ -1,45 +1,46 @@
-﻿// 
+﻿//Контрольная работа
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше или равна 3 символа
 
 string [] CreateArray ()
 {
     Console.Write ("Введите размер массива ");
     int length = Convert.ToInt32(Console.ReadLine());
     string [] array = new string [length];
-    Console.Write ("Введите элементы массива ");
+    Console.Write ($"Введите {length} элементов массива ");
     for (int i = 0; i < length; i++)
     {
         array[i] = ""+Console.ReadLine();
-        //Console.WriteLine(array[i] + " ");
     }
     return array;
 }
 
 string [] ChoiceArray (string [] array)
 {
-string [] newArray = new string [array.Length];
-int j=0;
+List<string> newArray = new List<string> ();
 for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3) 
         {
-            newArray [j] = array[i];
-            //Console.Write ("Значения нового массива " + newArray [j] + ", ");
-            j++;
+            newArray.Add(array[i]);
+            
         }
     }
-    return newArray;
+    return newArray.ToArray();
 }
 
 void PrintArray(string [] newArray)
 {
     Console.Write ("Значения нового массива ");
-    for (int i = 0; i < newArray.Length; i++)
+    Console.Write ("[ ");
+    for (int i = 0; i < newArray.Length-1; i++)
     {
-        Console.Write($"{newArray [i]}, ");
-               
+        Console.Write($"{newArray [i]}, ");     
     }
+    Console.Write($"{newArray [newArray.Length-1]}");
+    Console.Write (" ]");
 }
 
 string [] array = CreateArray ();
-ChoiceArray (array);
-PrintArray (array);
+string [] newArray = ChoiceArray (array);
+PrintArray (newArray);
